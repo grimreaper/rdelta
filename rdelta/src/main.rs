ef389ccc4af3c0321c47c39494c1932150af133a
Eitan Adler <lists@eitanadler.com>
2022-12-24 23:16:04 +0000

    [simplediff] add initial color output; it

--- rdelta/src/main.rs
+++ rdelta/src/main.rs
@@ -3,27 +3,29 @@ use itertools::{EitherOrBoth::*, Itertools};
 use unicode_segmentation::UnicodeSegmentation;
 
 fn simplediff(a: &str, b: &str) {
-    let combined = a.graphemes(true).zip(b.graphemes(true)).zip_longest();
+    let combined = a.graphemes(true).zip_longest(b.graphemes(true));
     for set in combined {
-        match combined {
+        match set {
             Both(l, r) => {
-                if (l == r) {
-                    print!(l)
+                if l == r {
+                    print!("{}", l)
                 } else {
-                    print!(l.red());
-                    print!(r.green());
+                    print!("{}", l.red());
+                    print!("{}",r.green());
                 }
             }
             Left(l) => {
-                print!(l.red());
+                print!("{}",l.red());
             }
             Right(r) => {
-                print!(r.green);
+                print!("{}", r.green());
             }
         }
     }
+    print!("\n")
 }
 
 fn main() {
     println!("Hello, world!");
+    simplediff("yolo world", "hello world");
 }
