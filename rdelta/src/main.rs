9709539c003e5f9460f18017c0c662ea75f25feb
Eitan Adler <lists@eitanadler.com>
2022-12-24 23:55:18 +0000

    [main] simplify conditional l==r

--- rdelta/src/main.rs
+++ rdelta/src/main.rs
@@ -5,20 +5,24 @@
 #![warn(missing_copy_implementations)]
 
 use colored::Colorize;
-use itertools::{EitherOrBoth::*, Itertools};
+use itertools::Itertools;
+use itertools::EitherOrBoth::{Left, Right, Both};
 use unicode_segmentation::UnicodeSegmentation;
+
+// fn outputdiff(str: EitherOrBoth<&str, &str>) {
+//
+// }
 
 fn simplediff(a: &str, b: &str) {
     let combined = a.graphemes(true).zip_longest(b.graphemes(true));
     for set in combined {
         match set {
+            Both(l, r) if l == r => {
+                print!("{}", l)
+            }
             Both(l, r) => {
-                if l == r {
-                    print!("{}", l)
-                } else {
-                    print!("{}", l.red());
-                    print!("{}",r.green());
-                }
+                print!("{}", l.red());
+                print!("{}",r.green());
             }
             Left(l) => {
                 print!("{}",l.red());
