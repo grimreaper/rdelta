bb4e7afc7eead2a332a174479247858fc5f738a2
Eitan Adler <lists@eitanadler.com>
2022-12-24 23:11:01 +0000

    [main] add initial string print to learn

--- rdelta/src/main.rs
+++ rdelta/src/main.rs
@@ -1,3 +1,29 @@
+use colored::Colorize;
+use itertools::{EitherOrBoth::*, Itertools};
+use unicode_segmentation::UnicodeSegmentation;
+
+fn simplediff(a: &str, b: &str) {
+    let combined = a.graphemes(true).zip(b.graphemes(true)).zip_longest();
+    for set in combined {
+        match combined {
+            Both(l, r) => {
+                if (l == r) {
+                    print!(l)
+                } else {
+                    print!(l.red());
+                    print!(r.green());
+                }
+            }
+            Left(l) => {
+                print!(l.red());
+            }
+            Right(r) => {
+                print!(r.green);
+            }
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
