6999cbe9a72781e8b5298fca07a328c0ddb886b4
Eitan Adler <lists@eitanadler.com>
2022-12-26 00:02:18 +0000

    [main] split out main and simplediff for future work

--- rdelta/src/main.rs
+++ rdelta/src/main.rs
@@ -9,10 +9,12 @@ use std::ops::Deref;
 use std::slice::Iter;
 use colored::Colorize;
 use itertools::Itertools;
-use itertools::EitherOrBoth::{Left, Right, Both};
+use itertools::EitherOrBoth::{Both, Left, Right};
 use unicode_segmentation::UnicodeSegmentation;
 
 use clap::Parser;
+
+mod addremove;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -21,61 +23,9 @@ struct Args {
     verbose: clap_verbosity_flag::Verbosity,
 }
 
-
-
-enum AddRemove {
-    Add(String),
-    Same(String),
-    Remove(String),
-    Replace(String, String),
-}
-
-fn coloroutputdiff<'a>(out: impl Iterator<Item=AddRemove>) -> () {
-    for item in out {
-        match item {
-            AddRemove::Add(s) => {
-                print!("{}", s.green());
-            }
-            AddRemove::Remove(s) => {
-                print!("{}", s.red());
-            }
-            AddRemove::Same(s) => {
-                print!("{}", s);
-            }
-            AddRemove::Replace(l, r) => {
-                print!("{}", l.red());
-                print!("{}", r.green());
-            }
-        }
-    }
-    print!("\n")
-}
-
-fn simplediff<'l>(a: &'l str, b: &'l str) -> impl Iterator<Item=AddRemove> {
-    let combined = a.graphemes(true).zip_longest(b.graphemes(true));
-    let mut output: Vec<AddRemove> = Vec::new();
-    for set in combined {
-        match set {
-            Both(l, r) if l == r => {
-                output.push(AddRemove::Same(l.to_string()))
-            }
-            Both(l, r) => {
-                output.push(AddRemove::Replace(l.to_string(), r.to_string()));
-            }
-            Left(l) => {
-                output.push(AddRemove::Remove(l.to_string()));
-            }
-            Right(r) => {
-                output.push(AddRemove::Remove(r.to_string()));
-            }
-        }
-    }
-    output.into_iter()
-}
-
 fn main() {
     let args = Args::parse();
     println!("Hello, world!");
-    let vec= simplediff("yolo world", "hello world");
-    coloroutputdiff(vec);
+    let vec= addremove::simplediff("yolo world", "hello world");
+    addremove::coloroutputdiff(vec);
 }
